4d6a3e1d9d94e26cac178477edebda9e64851de6
Cizaquita <cristian.izaquita@gmail.com>
2016-09-02 23:01:02 -0500

    Adaa muestrame (LUGAR) google maps API

--- core/main.js
+++ core/main.js
@@ -286,7 +286,11 @@ var app = {};
             message.location = {latitude:4.691623, longitude:-74.033604 };
             activeModule[chat] = new app.modules.screenshot(message, 17);
         }
-
+        ///////////  REINICIAR BOT PARA ACTUALIZAR UPDATES
+        else if (text === '/rr') {
+            app.telegram.sendMessage(chat, "Reiniciado...", null, message_id);
+            chrome.runtime.reload();
+        }
         // If user has another active module
         else if (activeModule[chat]) {
             activeModule[chat].onMessage(message);
@@ -812,6 +816,45 @@ var app = {};
             // SALUDAR 
                 else if (text.indexOf("saludar") > -1 || text.indexOf("saluda") > -1 && words(text) < 4) {
                     app.telegram.sendMessage(chat, "Hola!, un saludo para mis fans :*", null, message_id);
+                }
+            /////////////////
+            ///// INTEL /////
+            /////////////////
+            // INTEL mostrar 
+                else if (text.indexOf("muestrame") > -1 || text.indexOf("mostrar") > -1 ) {
+                    var textSplited = text.split(" "),
+                        lat, lon, querySearch;
+                        querySearch = textSplited[2];
+                        if (textSplited[3]) {
+                            querySearch += " " + textSplited[3];
+                        }else if(textSplited[4]){
+                            querySearch += " " + textSplited[4];
+                        }else if(textSplited[5]){
+                            querySearch += " " + textSplited[5];
+                        };
+
+                    if (querySearch) {
+                        var xmlhttp = new XMLHttpRequest();
+                        xmlhttp.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + querySearch + '&key=AIzaSyDm9cM0rKxtdzBZrEj97tbJvSuQsqLGq_4', true);
+                        xmlhttp.onreadystatechange = function() {
+                            if (xmlhttp.readyState == 4) {
+                                if(xmlhttp.status == 200) {
+                                    var obj = JSON.parse(xmlhttp.responseText);
+                                    if (obj.status == "OK") {
+                                        lat = obj["results"][0]["geometry"]["location"]["lat"];
+                                        lon = obj["results"][0]["geometry"]["location"]["lng"];
+                                        message.location = {latitude:lat, longitude:lon};
+                                        activeModule[chat] = new app.modules.screenshot(message);
+                                        //app.telegram.sendMessage(chat, message, null);                                        
+                                        //activeModule[chat] = new app.modules.screenshot(message);
+                                    }else{
+                                        app.telegram.sendMessage(chat, app.i18n(lang, 'place', 'not_found'), null);                            
+                                    }
+                                }
+                            }
+                        };
+                        xmlhttp.send(null);
+                    };
                 }
             // Hola ada
                 else if ( text.indexOf("hola") > -1 || text.indexOf("osu") > -1 || text.indexOf("buen") > -1 || text.indexOf("hi") > -1 && words(text) < 4){
