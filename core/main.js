518d5cb65923609ffba29330d6ff4dabd688afbf
Cizaquita <cristian.izaquita@gmail.com>
2016-09-05 21:54:45 -0500

    Google.load Feeds

--- core/main.js
+++ core/main.js
@@ -905,7 +905,7 @@ var app = {};
                     });
                 }
                 else if(text.indexOf("feed") > -1 && words(text) < 3){
-                    /*var feed = new google.feeds.Feed("https://fevgames.net/category/ingress/feed/");
+                    var feed = new google.feeds.Feed("https://fevgames.net/category/ingress/feed/");
                     var entradas;
                     feed.load(function(result) {
                         entradas = result.feed.entries;
@@ -916,7 +916,7 @@ var app = {};
                         });
 
                         app.telegram.sendMessage(chat, texto, null);
-                    });*/
+                    });
                 }
             // iluminada
                 else if(text.indexOf("iluminada") > -1 || text.indexOf("enlightened") > -1 && words(text) < 5){
