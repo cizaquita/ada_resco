86f375552ba241a4dc8f51a6f8365739c84ca755
RATAELTRIFORCE <rafael.el.fisico@gmail.com>
2016-09-06 02:53:46 -0500

    Intento inutil 2

--- core/distancecalc.module.js
+++ core/distancecalc.module.js
@@ -73,8 +73,8 @@
                             ["Softbank Ultra Link"],
                             ["Ninguno"]
                         ];
-                        var suma = (this.resos[0] + this.resos[1] + this.resos[2] + this.resos[3] +
-                                      this.resos[4] + this.resos[5] + this.resos[6] + this.resos[7]);
+                        var suma = (Number(this.resos[0]) + Number(this.resos[1]) + Number(this.resos[2]) + Number(this.resos[3]) +
+                                      Number(this.resos[4]) + Number(this.resos[5]) + Number(this.resos[6]) + Number(this.resos[7]));
                         var elevar = Math.pow(suma, 4);
                         resultado = elevar * 160;
                         app.telegram.sendMessage(this.chat, "Resonadores: " +
